0e30946f1aae2c0d27343029176af503fc72566a
Carlito Murta (Hanzo) <carlitomurtaramalho@gmail.com>
2018-10-21 02:34:44 -0300

    initial config

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Ola react native</Text>
+      </View>
+    );
+  }
+}
